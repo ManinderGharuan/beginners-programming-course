589eb302b4f3efbbd34034e182385564ea8528bb
Maninder Singh <maninderrai7@gmail.com>
2017-01-06 18:11:54 +0530

    Add assignment1 code

---
+++ assignments/assignment1.py
@@ -0,0 +1,19 @@
+def is_prime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+def get_primes(num):
+    primes = []
+
+    for i in range(num + 1):
+        if is_prime(i):
+            primes.append(i)
+
+    return primes
