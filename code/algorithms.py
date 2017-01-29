22ea14f2cca32bbd1b972737a467d87a372debb8
Maninder Singh <maninderrai7@gmail.com>
2017-01-29 10:44:07 +0530

    Add merge sort Algorithm code

--- code/algorithms.py
+++ code/algorithms.py
@@ -41,3 +41,38 @@ def bubble_sort(ls, have_changed=True):
             have_changed = True
 
     return bubble_sort(ls, have_changed)
+
+
+def merge(left, right):
+    """joins sorted list left and right return new sorted list"""
+    merge_list = []
+    i, j = 0, 0
+
+    while len(left) + 1 > i:
+        if j == len(right):
+            merge_list.extend(left[i:])
+            break
+
+        if i == len(left):
+            merge_list.extend(right[j:])
+            break
+
+        if left[i] > right[j]:
+            merge_list.append(right[j])
+            j += 1
+        else:
+            merge_list.append(left[i])
+            i += 1
+
+    return merge_list
+
+
+def merge_sort(ls):
+    if len(ls) < 2:
+        return ls
+
+    mid = len(ls) // 2
+    left = merge_sort(ls[:mid])
+    right = merge_sort(ls[mid:])
+
+    return merge(left, right)
