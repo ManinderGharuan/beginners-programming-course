341d27fdfe7242d4d20fcdb9b8d37b53de833760
Maninder Singh <maninderrai7@gmail.com>
2017-02-17 18:33:04 +0530

    Make Assignment9 test in Classes

--- assignments/test_assignment9.py
+++ assignments/test_assignment9.py
@@ -2,88 +2,86 @@ from assignment9 import (Circle,
                          Square,
                          Triangle,
                          ShapeSet,
-                         findLargest,
-                         readShapesFromFile)
+                         findLargest)
 
 
-t1 = Triangle(3, 6)
-t2 = Triangle(2, 6)
-t3 = Triangle(5, 3)
-t4 = Triangle(7, 2)
-t5 = Triangle(2, 6)
+class TestTriangle():
+    def setup(self):
+        self.t1 = Triangle(3, 6)
+        self.t2 = Triangle(2, 6)
+        self.t3 = Triangle(5, 3)
+        self.t4 = Triangle(7, 2)
+        self.t5 = Triangle(2, 6)
+
+    def test_area(self):
+        assert self.t1.area() == 9
+        assert self.t2.area() == 6
+        assert self.t3.area() == 7.5
+        assert self.t4.area() == 7
+
+    def test_str(self):
+        assert str(self.t1) == 'Triangle with base 3.0 and height 6.0'
+        assert str(self.t3) == 'Triangle with base 5.0 and height 3.0'
+
+    def test_eq(self):
+        assert (self.t1 == self.t2) is False
+        assert (self.t2 == self.t5) is True
+        assert (self.t3 == self.t4) is False
 
 
-def test_Triangle_area():
-    assert t1.area() == 9
-    assert t2.area() == 6
-    assert t3.area() == 7.5
-    assert t4.area() == 7
+class TestShapeSet():
+    def setup(self):
+        self.ss1 = ShapeSet()
+        self.t11 = Triangle(3, 6)
+        self.t12 = Triangle(5, 2)
+        self.c11 = Circle(4)
+        self.c12 = Circle(8)
+        self.s11 = Square(5)
+        self.s12 = Square(6)
+        self.s13 = Square(5)
+        self.ss1.addShape(self.t11)
+        self.ss1.addShape(self.t12)
+        self.ss1.addShape(self.c12)
+        self.ss1.addShape(self.c11)
+        self.ss1.addShape(self.s11)
+        self.ss1.addShape(self.s12)
+        self.ss1.addShape(self.s13)
+        self.l1 = [self.t11, self.t12, self.c12, self.c11, self.s11, self.s12]
 
+        self.ss2 = ShapeSet()
+        self.t21 = Triangle(2.0, 7)
+        self.t22 = Triangle(2.0, 7)
+        self.t23 = Triangle(4, 2)
+        self.c21 = Circle(4)
+        self.c22 = Circle(4)
+        self.s21 = Square(9)
+        self.s22 = Square(2)
+        self.s23 = Square(2)
+        self.s24 = Square(6)
+        self.ss2.addShape(self.t21)
+        self.ss2.addShape(self.t22)
+        self.ss2.addShape(self.c22)
+        self.ss2.addShape(self.c21)
+        self.ss2.addShape(self.s21)
+        self.ss2.addShape(self.s22)
+        self.ss2.addShape(self.s23)
+        self.l2 = [self.t21, self.c22, self.s21, self.s22]
 
-def test_Triangle_str():
-    assert str(t1) == 'Triangle with base 3.0 and height 6.0'
-    assert str(t3) == 'Triangle with base 5.0 and height 3.0'
+    def test_addShape(self):
+        assert (self.t11 in self.ss1) is True
+        assert (self.s22 in self.ss2) is True
 
+    def test_iter(self):
+        assert all(x in self.ss1 for x in self.l1) is True
 
-def test_Triangle_eq():
-    assert (t1 == t2) is False
-    assert (t2 == t5) is True
-    assert (t3 == t4) is False
+        for (index, value) in enumerate(self.ss2):
+            assert (value == self.l2[index]) is True
 
-
-ss1 = ShapeSet()
-
-t11 = Triangle(3, 6)
-t12 = Triangle(5, 2)
-c11 = Circle(4)
-c12 = Circle(8)
-s11 = Square(5)
-s12 = Square(6)
-s13 = Square(5)
-
-ss1.addShape(t11)
-ss1.addShape(t12)
-ss1.addShape(c12)
-ss1.addShape(c11)
-ss1.addShape(s11)
-ss1.addShape(s12)
-ss1.addShape(s13)
-
-ss2 = ShapeSet()
-
-t21 = Triangle(2.0, 7)
-t22 = Triangle(2.0, 7)
-c21 = Circle(4)
-c22 = Circle(4)
-s21 = Square(9)
-s22 = Square(2)
-s23 = Square(2)
-
-ss2.addShape(t21)
-ss2.addShape(t22)
-ss2.addShape(c22)
-ss2.addShape(c21)
-ss2.addShape(s21)
-ss2.addShape(s22)
-ss2.addShape(s23)
-
-
-def test_ShapeSet_addShape():
-    assert ss1.shape_set == [t11, t12, c12, c11, s11, s12]
-    assert ss2.shape_set == [t21, c22, s21, s22]
-
-
-def test_ShapeSet_str():
-    assert str(ss1) == "{}\n{}\n{}\n{}\n{}\n{}" .format(t11,
-                                                        t12,
-                                                        c12,
-                                                        c11,
-                                                        s11,
-                                                        s12)
-    assert str(ss2) == "{}\n{}\n{}\n{}" .format(t21,
-                                                c22,
-                                                s21,
-                                                s22)
+    def test_str(self):
+        assert str(self.ss1) == "{}\n{}\n{}\n{}\n{}\n{}" \
+            .format(self.t11, self.t12, self.c12, self.c11, self.s11, self.s12)
+        assert str(self.ss2) == "{}\n{}\n{}\n{}" \
+            .format(self.t21, self.c22, self.s21, self.s22)
 
 
 def test_findLargest():
